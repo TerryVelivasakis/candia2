8d0921f7b844376845b1509a8d85ebaa0ec96df7
terry <terry@candiaholdings.com>
2021-06-29 23:25:23 -0400

    bug fixes

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,3 +1,9 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.0.1/dist/flatly/bootstrap.min.css" integrity="sha256-n/rdSlkUha+pkgKSz9eM/yAE3jGWaOPpS+BN1Jfm/iA=" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous">
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="undefined" crossorigin="anonymous"></script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
-<?php require 'includes/loadme.php';?>
+<?php require $_SERVER["DOCUMENT_ROOT"].'/includes/loadme.php'?>
 
 <h1><a href="\leasing\executiveLease.php" target="_blank">executive lease</a></h1>
+<h1><a href="\leasing\letters\mailmerge.php" target="_blank">Termination</a></h1>
